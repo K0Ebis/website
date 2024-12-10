d3de58ee0f3d2f6f9641ae0cb9d9cee0790812e0
K0Ebis <cyberjobgt.ok@gmail.com>
2024-12-10 11:53:54 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("p").textContent = "Hello World!!";
+window.alert("こうやって刻み込む───『Hello World!!』");
+console.log("やっほ、イオリ！");
+var array = ["apple", "banana", "lemon", "orange"];
